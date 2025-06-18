671b7e10461384b2c18b0bc577609d79f5fe7670
Sreehari <sreeharims2000@gmail.com>
2025-06-18 23:50:30 +0530

    Test Suite: Exact word match v1

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,1 +1,15 @@
-pass
+def test_basic_match():
+    engine = RegexEngine("basic")
+    assert engine.match_current_line("this is a basic test")
+
+def test_no_match():
+    engine = RegexEngine("no-basic")
+    assert engine.match_current_line("this is not a basic test")
+
+def test_empty_pattern():
+    engine = RegexEngine("")
+    assert engine.match_current_line("I think it is a match")
+
+def test_exact_match():
+    engine = RegexEngine("basic")
+    assert engine.match_current_line("this is another basic test")
