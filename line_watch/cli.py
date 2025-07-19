ca8eb564c94cc8053b5a16b1aba28e4064865e2d
Sreehari <sreeharims2000@gmail.com>
2025-07-19 11:25:34 +0530

    PyPI-ready release

--- line_watch/cli.py
+++ line_watch/cli.py
@@ -1,18 +1,12 @@
-import sys
-from line_watch.engine import RegexEngine
-
 def entry_point():
     if len(sys.argv) != 3:
         sys.exit(1)
-    
+
     pattern = sys.argv[1]
     line_text = sys.argv[2]
 
     engine = RegexEngine(pattern)
-    if engine.match_current_line(text):
+    if engine.match_current_line(line_text):
         print("✓ - Matched")
     else:
         print("X - Not Matched")
-
-if __name__ == "__main__":
-    entry_point()
